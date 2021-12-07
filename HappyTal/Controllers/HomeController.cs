40bf057f892816eb296ae010140bf010583f49c7
AxiomMT <61475769+AxiomMT@users.noreply.github.com>
2021-12-07 18:05:40 +0100

    Feature/factory page (#1)
    ## Explanation
    
    Creation of a factory monitor panel to visualize the different stages number of cakes
    
    ## What we did
    
    - We created **CakeFactory page** and relatives
      - CakeFactory.cshtml html page
      - cakeFactory.css stylesheet
      - cakeFactory.js script file
    
    - We made a Monitor panel with: 
      - a Monitoring section displaying three values: Preparation, Cuisson, Emballage
      - a control section with two buttons: Start factory and Stop factory

--- HappyTal/Controllers/HomeController.cs
+++ HappyTal/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace HappyTal.Controllers
             return View();
         }
 
+        public IActionResult CakeFactory()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
